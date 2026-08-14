9b3038a32212abce92849f656500b0d8033ae9e8
Augusto Freitas <162934392+IgutoI@users.noreply.github.com>
2026-08-14 11:37:46 -0400

    Add LivroIndisponivelException class

---
+++ Library ManagementSystem/src/br/com/biblioteca/exception/LivroIndisponivelException.java
@@ -0,0 +1,7 @@
+package br.com.biblioteca.exception;
+
+public class LivroIndisponivelException extends RuntimeException{
+    public LivroIndisponivelException(String mensagem){
+        super(mensagem);
+    }
+}
